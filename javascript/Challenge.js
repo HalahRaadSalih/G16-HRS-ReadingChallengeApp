c49477108a5db235b2fe8678d8156b29f825cea8
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-11-19 15:37:23 -0800

    added skeleton for the challenge object

---
+++ javascript/Challenge.js
@@ -0,0 +1,29 @@
+/*
+	Challenge object has the following variables
+	name : the name of the challenge,
+	description: the challenge description
+	duration : how long it takes to finish the challenge,
+	books : the list of books 
+*/
+var Challenge = function(name, description, duration){
+	this.name = name;
+	this.description = description;
+	this.duration = duration;
+}
+
+
+Challenge.prototype.addBook = function(first_argument) {
+	// body...
+};
+
+Challenge.prototype.removeBook = function(){
+
+}
+
+Challenge.prototype.status = function(first_argument) {
+	// body...
+};
+
+Challenge.prototype.finish = function(){
+
+}
