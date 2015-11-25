a5cf4ff2181ea25e175f903df4f45878ae4ce87f
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-11-25 15:02:19 -0800

    added user object skeleton

---
+++ javascript/User.js
@@ -0,0 +1,21 @@
+var User = function(firstName, lastName, email){
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.challenges = [];
+	this.finishedChallenges = [];
+	this.unfinishedCallenges = [];
+}
+
+
+User.prototype.addChallenge = function(first_argument) {
+	
+};
+
+User.prototype.removeChallenge = function(first_argument) {
+	
+};
+
+User.prototype.editChallenge = function(first_argument) {
+	
+};
