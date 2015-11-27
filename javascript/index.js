2d3eed1eed84462ca8cad7f2756e47d6dc03c341
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-11-27 00:12:24 -0800

    implemented user logout

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,8 @@ $("#newChallenge").on('click', function(){
     	window.location.replace("createChallenge.html");
 });
 
+
+$("#logoutUser").on('click', function(){
+	ref.unauth();
+	window.location.replace("login.html");
+});
